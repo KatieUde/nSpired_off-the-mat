4a20fd41834d64746c0b56941e09eca7aa2528d8
Katherine Ude <Katie.Ude@gmail.com>
2016-01-19 17:53:55 -0600

    added secure passwords to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 75 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    # infers uniqueness will be true
-    uniqueness: { case_sensitive: false }
+      format: { with: VALID_EMAIL_REGEX },
+      # infers uniqueness will be true
+      uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
