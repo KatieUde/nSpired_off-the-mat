c302389fc2c9e476537dc18052055256f4c293a5
Katherine Ude <Katie.Ude@gmail.com>
2016-01-20 12:52:49 -0600

    saving ability to view user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -47,6 +51,7 @@ class UsersController < ApplicationController
     # confirms a logged-in user
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in to edit profile!"
         redirect_to login_url
     end
