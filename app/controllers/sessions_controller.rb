31caf7f3ce7dbd84f54b803e1ed5850098b60808
Katherine Ude <Katie.Ude@gmail.com>
2016-01-20 16:47:11 -0600

    account activation should be completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,22 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
         # Log in the user and redirect to the user's show page
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
-      else
+        if user.activated?
+          log_in user
+          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+          redirect_back_or user
+        else
+          message = "Account not activated. "
+          message += "Check your email for the activation link."
+          flash[:warning] = message
+          redirect_to root_url
+        end
         # Create an error message
-      flash.now[:danger] = 'Invalid email & password combination'
-      render 'new'
+      else
+        flash.now[:danger] = 'Invalid email & password combination'
+        render 'new'
+      end
     end
-  end
 
   def destroy
     log_out if logged_in?
