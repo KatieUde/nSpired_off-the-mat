d6c68d79c7c8a5fa85af5d0168ef02c0df679a6b
Katherine Ude <Katie.Ude@gmail.com>
2016-01-25 22:52:28 -0600

    meditation modals working now

--- app/assets/javascripts/meditations.js
+++ app/assets/javascripts/meditations.js
@@ -1,2 +1,30 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function popZen() {
+  $("#zen").hover(function() {
+    $("#zen-deat.ui.modal").modal('show');
+    console.log('stuff is happening');
+  });
+}
+
+function popVip() {
+  $("#vip").hover(function() {
+    $("#vip-deat.ui.modal").modal('show');
+    console.log('stuff is happening');
+  });
+}
+
+function popMind() {
+  $("#mind").hover(function() {
+    $("#mind-deat.ui.modal").modal('show');
+    console.log('stuff is happening');
+  });
+}
+
+function popLove() {
+  $("#love-kind").hover(function() {
+    $("#love-kind-deat.ui.modal").modal('show');
+    console.log('stuff is happening');
+  });
+};
